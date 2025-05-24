ca20c73d7673ea510937ec85bf330469fb316b6e
Shani <shanitk@mta.ac.il>
2025-05-24 12:16:04 +0300

    changes

--- routes/pages.js
+++ routes/pages.js
@@ -1,26 +1,38 @@
-const express=require('express');
-const path=require('path');
-const router=express.Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
 
+// Controllers
 const searchController = require('../Controllers/searchController');
 const profileController = require('../Controllers/profileController');
+
+// דף הבית
+router.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'views', 'index.html'));
+});
+
+// עמוד פרופיל
+router.get('/profile_page', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'views', 'profile_page.html'));
+});
+
+// עמוד גלילת פרופילים
+router.get('/scroll_profiles', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'views', 'scroll_profiles.html'));
+});
+
+// עדכון פרופיל
 router.post('/save-profile', profileController.updateProfile);
 
+// תצוגת פרופיל לעריכה
+router.get('/edit-profile', profileController.updateProfile); // שימי לב - ייתכן שצריך לשנות לפונקציה נפרדת לעריכה
 
+// חיפוש פרופילים
 router.get('/search', searchController.searchProfile);
-router.get('/edit-profile', profileController.updateProfile);
-router.get('/', (req, res) => {
-    res.sendFile(path.join("../views/index.html"));
-});
-router.get('/profile_page', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'profile_page.html'));
-});
-router.get('/scroll_profiles', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'scroll_profiles.html'));
-});
+
+// עמוד 404 לכל נתיב אחר
 router.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'file_not_found.html'));
+  res.status(404).sendFile(path.join(process.cwd(), 'views', 'file_not_found.html'));
 });
 
-module.exports=router;
-
+module.exports = router;
